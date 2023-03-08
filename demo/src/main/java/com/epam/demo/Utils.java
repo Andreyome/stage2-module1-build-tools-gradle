c597ed3723f625de07a5a9f034d8e9e8320a59a0
Andreyome <andreyomem@gmail.com>
2023-03-08 23:22:49 +0200

    fix 3

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,12 +1,16 @@
 package com.epam.demo;
 
-import java.security.spec.ECField;
+import org.apache.commons.lang3.math.NumberUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         try {
             for (String str : args) {
+                if(!NumberUtils.isNumber(str)){
+                    return false;
+                }
                 if (Float.parseFloat(str) <= 0) {
                     return false;
                 }
