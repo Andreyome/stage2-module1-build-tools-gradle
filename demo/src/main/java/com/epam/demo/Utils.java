69f49c80e4b71f85c89bcbdc56f742a5940f4b4f
Andreyome <andreyomem@gmail.com>
2023-03-09 00:07:23 +0200

    fix last

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -8,6 +8,9 @@ public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         try {
             for (String str : args) {
+                if(str.startsWith("01")||str.startsWith("02") || str.startsWith("03") || str.startsWith("04") || str.startsWith("05") || str.startsWith("06") || str.startsWith("07") || str.startsWith("08") || str.startsWith("09")){
+                    return false;
+                }
                 if(!NumberUtils.isNumber(str)){
                     return false;
                 }
